b040d800ed6d60dec414e2f0f09a86a542969fb5
rdpl0740 <rdpl0740@leeds.ac.uk>
2025-10-22 19:07:52 +0000

    Added all functions

--- portfolio/Wordle.kt
+++ portfolio/Wordle.kt
@@ -3,12 +3,17 @@
 • Pick a random word from this list
 • Repeatedly prompt the user to guess the chosen word
 • Give feedback on which letters have been guessed correctly*/
-public MutableList<String> wordList = mutableListOf<string>()
+
+public 
+public 
 
 fun main() 
-{
-    wordList = readWordList("portfolio/wordle/data/words")
-    
+{   
+    //Variable initialisation
+
+    MutableList<String> wordList = readWordList("portfolio/wordle/data/words")
+    String wordle = ""
+
 }
 
 fun readWordList(filename: String): MutableList<String>
@@ -21,11 +26,70 @@ fun readWordList(filename: String): MutableList<String>
 
 fun isValid(word: String): Boolean
 {
-    if(wordList.contains(word))
+    if( word.length == 5 )
     {
         return true
     }
-    
+
     return false
 }
     
+fun pickRandomWord(words: MutableList<String>): String
+{
+    String word = words[Random.nextInt( 0 , words.size - 1)]
+    
+    return word
+}
+
+fun obtainGuess(attempt: Int): String
+{
+    println("Attempt"+attempt+":")  //Print attempt number
+    String guess = readLine()
+
+    while(!isValid(guess))  //Loop message and input
+    {
+        println("Try again.")
+        guess = readLine()
+    }
+    
+    return guess
+}
+
+fun evaluateGuess(guess: String, target: String): List<Int>
+{
+    MutableList evaluation = mutableListOf<Int>()
+
+    for (i in 0..4) 
+    {
+        if(guess[i] == target[i])
+        {
+            evaluation[i] = 1
+        }
+        else
+        {
+            evaluation[i] = 0
+        }
+    }
+
+    return evaluation
+}
+
+fun displayGuess(guess: String, matches: List<Int>)
+{
+    String output = ""
+
+    for (i in 0..4) {
+
+        if(matches[i] = 1)
+        {
+            output += guess[i]
+        }
+        else
+        {
+            output += "?"
+        }
+
+    }
+
+    return output
+}
